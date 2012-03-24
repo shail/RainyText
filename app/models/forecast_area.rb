76596159f5b0f03a25a443513d96098673dc4a7f
Shail Patel <shailpatel2@gmail.com>
2012-03-24 12:28:35 -0700

    fixes

--- app/models/forecast_area.rb
+++ app/models/forecast_area.rb
@@ -3,9 +3,9 @@ class ForecastArea < ActiveRecord::Base
 	validates :zipcode, :presence => true
 	validates :phone_number, :presence => true
 	validates :time, :presence => true
+	validates :forecast_time_zone, :presence => true
 	validates :zipcode, :length => { :is => 5 }
 	validates :phone_number, :length => { :is => 10}
-	validates :forecast_time_zone, :presence => true
 	belongs_to :user
 
 end
