e1b71f085346cfcea4bf94285b34df4ddb4b6f65
Shail Patel <shailpatel2@gmail.com>
2012-03-25 03:04:12 -0700

    some tooltips added

---
+++ app/assets/javascripts/form.js
@@ -0,0 +1,2 @@
+$("#forecast_area_zipcode").tooltip({title: 'e.g. 03862', placement: 'right', trigger: 'focus'});
+$("#forecast_area_phone_number").tooltip({title: 'e.g 6504218549', placement: 'right', trigger: 'focus'})
