5c3dc0a08591f28e192114e8f92bfb0b2316853d
Shail Patel <shailpatel2@gmail.com>
2012-03-26 22:56:48 -0700

    callback stuff

--- app/models/forecast_area.rb
+++ app/models/forecast_area.rb
@@ -7,23 +7,23 @@ class ForecastArea < ActiveRecord::Base
 	validates :zipcode, :length => { :is => 5 }
 	validates :phone_number, :length => { :is => 10}
 	belongs_to :user
-	before_create :generate_and_send_verify_code
+	# before_create :generate_and_send_verify_code
 
 	protected
 
-	def generate_and_send_verify_code
-		self.verify_code = SecureRandom.hex(5)
+	# def generate_and_send_verify_code
+	# 	self.verify_code = SecureRandom.hex(5)
 
-    account_sid = 'AC1e33ca9e05cd4f86b12ffab6078464f6'
-    auth_token = 'c451f2e1dc06ae831b8c542323099101'
+ #    account_sid = 'AC1e33ca9e05cd4f86b12ffab6078464f6'
+ #    auth_token = 'c451f2e1dc06ae831b8c542323099101'
 
-    client = Twilio::REST::Client.new account_sid, auth_token
+ #    client = Twilio::REST::Client.new account_sid, auth_token
     
-    client.account.sms.messages.create(
-      :from => '+17273466568',
-      :to => "+1#{self.phone_number}",
-      :body => "#{self.verify_code}"
-    )
-  end
+ #    client.account.sms.messages.create(
+ #      :from => '+17273466568',
+ #      :to => "+1#{self.phone_number}",
+ #      :body => "#{self.verify_code}"
+ #    )
+ #  end
 end
 
